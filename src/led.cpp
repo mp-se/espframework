c09a7d9048d441f6efe6f07403469aac440b24ad
Magnus <mp-se@users.noreply.github.com>
2024-05-25 23:14:43 +0200

    Refactor

--- src/led.cpp
+++ src/led.cpp
@@ -21,6 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+#include <Ticker.h>
+
 #include <espframework.hpp>
 #include <led.hpp>
 #include <log.hpp>
@@ -38,8 +40,6 @@ void ledOn(LedColor l) {
   neopixelWrite(pin, r, g, b);
 }
 #else
-#include <Ticker.h>
-
 bool ledInit = false;
 Ticker ledTicker;
 
