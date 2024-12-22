7205c1efaaec98d2baa73115402c746252f20fc7
Magnus <mp-se@noreply.github.com>
2024-12-22 16:15:18 +0100

    Refactor to use std pointer

--- src/basewebserver.hpp
+++ src/basewebserver.hpp
@@ -46,6 +46,7 @@ SOFTWARE.
 #include <espframework.hpp>
 #include <interface.hpp>
 #include <log.hpp>
+#include <memory>
 
 #if defined(ESP8266)
 #include <incbin.h>
@@ -66,7 +67,7 @@ extern const uint8_t faviconIcoEnd[] asm("_binary_html_favicon_ico_gz_end");
 
 class BaseWebServer {
  protected:
-  AsyncWebServer *_server = 0;
+  std::unique_ptr<AsyncWebServer> _server;
   File _uploadFile;
   WebConfig *_webConfig;
   int _uploadReturn = 200;
@@ -186,7 +187,7 @@ class BaseWebServer {
                          int dynamicJsonSize = JSON_BUFFER_SIZE_L);
 
   virtual bool setupWebServer();
-  virtual AsyncWebServer *getWebServer() { return _server; }
+  virtual AsyncWebServer *getWebServer() { return _server.get(); }
   virtual void setWifiSetup(bool wifiSetup) { _wifiSetup = wifiSetup; }
   virtual void loop();
 };
