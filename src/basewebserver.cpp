c55f9a4e72de1bf76c2a366715925038b6158c2a
Magnus <mp-se@noreply.github.com>
2025-12-20 16:22:17 +0100

    Update to async webserver 3.9

--- src/basewebserver.cpp
+++ src/basewebserver.cpp
@@ -458,19 +458,17 @@ void BaseWebServer::setupWebHandlers() {
 
   _server->on(
       "/api/auth", HTTP_GET,
-      std::bind(&BaseWebServer::webHandleAuth, this, std::placeholders::_1));
+      [this](AsyncWebServerRequest *request) { this->webHandleAuth(request); });
   _server->on("/api/wifi/status", HTTP_GET,
-              std::bind(&BaseWebServer::webHandleWifiScanStatus, this,
-                        std::placeholders::_1));
+              [this](AsyncWebServerRequest *request) { this->webHandleWifiScanStatus(request); });
   _server->on("/api/wifi", HTTP_GET,
-              std::bind(&BaseWebServer::webHandleWifiScan, this,
-                        std::placeholders::_1));
+              [this](AsyncWebServerRequest *request) { this->webHandleWifiScan(request); });
   _server->on(
       "/api/restart", HTTP_GET,
-      std::bind(&BaseWebServer::webHandleRestart, this, std::placeholders::_1));
+      [this](AsyncWebServerRequest *request) { this->webHandleRestart(request); });
   _server->on(
       "/api/ping", HTTP_GET,
-      std::bind(&BaseWebServer::webHandlePing, this, std::placeholders::_1));
+      [this](AsyncWebServerRequest *request) { this->webHandlePing(request); });
   _server->on(
       "/api/filesystem/upload", HTTP_POST,
       std::bind(&BaseWebServer::webReturnOK, this, std::placeholders::_1),
